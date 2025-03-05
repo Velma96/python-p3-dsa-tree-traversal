8243e1616c7abfa694ee44d87f0802e61021fc24
phoebe velma awuor <awuorphoebi@gmail.com>
2025-03-05 09:39:37 +0300

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,30 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        return self._dfs(self.root, target_id)
+
+    def _dfs(self, node, target_id):
+        if node['id'] == target_id:
+            return node
+        
+        for child in node['children']:
+            result = self._dfs(child, target_id)
+            if result:
+                return result
+
+        return None
+    def get_element_by_id(self, target_id):
+        nodes_to_visit = [self.root]
+
+        while nodes_to_visit:
+            node = nodes_to_visit.pop(0)
+
+            if node['id'] == target_id:
+                return node
+
+            nodes_to_visit.extend(node['children'])
+
+        return None
+
